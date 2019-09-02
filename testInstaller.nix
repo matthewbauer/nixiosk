595989a34a2390e3e9ec5fe672381b8f2003ea20
Ryan Trinkle <ryan.trinkle@gmail.com>
2019-09-02 09:14:25 -0400

    Make installer test VM work without network connectivity

--- testInstaller.nix
+++ testInstaller.nix
@@ -1,10 +1,22 @@
 (import <nixpkgs/nixos> {
   system = "x86_64-linux";
-  configuration = { config, ... }: {
-    imports = [ (<nixpkgs> + /nixos/modules/installer/cd-dvd/installation-cd-minimal.nix) ];
+  configuration = { config, pkgs, lib, ... }: {
+    imports = [
+      (<nixpkgs> + /nixos/modules/installer/cd-dvd/installation-cd-minimal.nix)
+    ];
     boot.kernelParams = [ "console=ttyS0" ];
     environment.sessionVariables = {
-      BASALT_GIT_HOOKS = "${./git-hooks}";
+      # This is just to ensure it gets included in the installer's nix store so we don't need to access the network
+      TARGET_PIN = builtins.toString (import <nixpkgs/nixos> { configuration = import ./testTarget.nix; }).system;
     };
+    isoImage.includeSystemBuildDependencies = true;
+    environment.systemPackages = with pkgs; [
+      git
+      gitAndTools.git-subrepo
+    ];
+    nix.binaryCaches = lib.mkForce [ "auto?trusted=1" ];
+    nix.extraOptions = ''
+      fallback = true
+    '';
   };
 }).config.system.build.isoImage
