7b013e36a6c4f48806b2b855a197e7d07501ab12
Matthew Bauer <mjbauer95@gmail.com>
2020-03-06 22:43:13 -0500

    Squashed 'basalt/' content from commit 2fe06be
    git-subtree-dir: basalt
    git-subtree-split: 2fe06be91971edb90156a3f4f2a2103a884bb80a

---
+++ test/nixos/testInstaller.nix
@@ -0,0 +1,21 @@
+(import <nixpkgs/nixos> {
+  system = "x86_64-linux";
+  configuration = { config, pkgs, lib, ... }: {
+    imports = [
+      (<nixpkgs> + /nixos/modules/installer/cd-dvd/installation-cd-minimal.nix)
+    ];
+    boot.kernelParams = [ "console=ttyS0" ];
+    environment.sessionVariables = {
+      # This is just to ensure it gets included in the installer's nix store so we don't need to access the network
+      TARGET_PIN = builtins.toString (import <nixpkgs/nixos> { configuration = import ./testTarget.nix; }).system;
+    };
+    isoImage.includeSystemBuildDependencies = true;
+    environment.systemPackages = with pkgs; [
+      git
+    ];
+    nix.binaryCaches = lib.mkForce [ "auto?trusted=1" ];
+    nix.extraOptions = ''
+      fallback = true
+    '';
+  };
+}).config.system.build.isoImage
