a93062404cb8d7828e9e3a28fbbd6d926485e89a
Matthew Bauer <mjbauer95@gmail.com>
2020-05-01 15:13:58 -0500

    Built actual images

--- release.nix
+++ release.nix
@@ -38,78 +38,78 @@ in
     name = "retroPi0";
     hardware = "raspberryPi0";
     program = { package = "retroarch"; executable = "/bin/retroarch"; };
-  }).config.system.build.toplevel;
+  }).config.system.build.sdImage;
 
   retroPi4 = (boot {
     name = "retroPi4";
     hardware = "raspberryPi4";
     program = { package = "retroarch"; executable = "/bin/retroarch"; };
-  }).config.system.build.toplevel;
+  }).config.system.build.sdImage;
 
   retroOva = (boot {
     name = "retroOva";
     hardware = "ova";
     program = { package = "retroarch"; executable = "/bin/retroarch"; };
-  }).config.system.build.toplevel;
+  }).config.system.build.virtualBoxOVA;
 
   retroIso = (boot {
     name = "retroIso";
     hardware = "iso";
     program = { package = "retroarch"; executable = "/bin/retroarch"; };
-  }).config.system.build.toplevel;
+  }).config.system.build.isoImage;
 
   epiphanyPi0 = (boot {
     name = "epiphanyPi0";
     hardware = "raspberryPi0";
     program = { package = "epiphany"; executable = "/bin/epiphany"; };
-  }).config.system.build.toplevel;
+  }).config.system.build.sdImage;
 
   epiphanyPi4 = (boot {
     name = "epiphanyPi4";
     hardware = "raspberryPi4";
     program = { package = "epiphany"; executable = "/bin/epiphany"; };
-  }).config.system.build.toplevel;
+  }).config.system.build.sdImage;
 
   demoPi0 = (boot {
     name = "demoPi0";
     hardware = "raspberryPi0";
     program = { package = "gtk3"; executable = "/bin/gtk3-demo"; };
-  }).config.system.build.toplevel;
+  }).config.system.build.sdImage;
 
   demoPi1 = (boot {
     name = "demoPi1";
     hardware = "raspberryPi1";
     program = { package = "gtk3"; executable = "/bin/gtk3-demo"; };
-  }).config.system.build.toplevel;
+  }).config.system.build.sdImage;
 
   demoPi2 = (boot {
     name = "demoPi2";
     hardware = "raspberryPi2";
     program = { package = "gtk3"; executable = "/bin/gtk3-demo"; };
-  }).config.system.build.toplevel;
+  }).config.system.build.sdImage;
 
   demoPi3 = (boot {
     name = "demoPi3";
     hardware = "raspberryPi3";
     program = { package = "gtk3"; executable = "/bin/gtk3-demo"; };
-  }).config.system.build.toplevel;
+  }).config.system.build.sdImage;
 
   demoPi4 = (boot {
     name = "demoPi4";
     hardware = "raspberryPi4";
     program = { package = "gtk3"; executable = "/bin/gtk3-demo"; };
-  }).config.system.build.toplevel;
+  }).config.system.build.sdImage;
 
   kodiPi4 = (boot {
     name = "kodiPi4";
     hardware = "raspberryPi4";
     program = { package = "kodi"; executable = "/bin/kodi"; };
-  }).config.system.build.toplevel;
+  }).config.system.build.sdImage;
 
   kodiOva = (boot {
     name = "kodiOva";
     hardware = "ova";
     program = { package = "kodi"; executable = "/bin/kodi"; };
-  }).config.system.build.toplevel;
+  }).config.system.build.virtualBoxOVA;
 
 }
