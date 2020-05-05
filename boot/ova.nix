8ac98925c37e23fa1670aac824956f77a5815dae
Matthew Bauer <mjbauer95@gmail.com>
2020-05-05 00:24:53 -0500

    Set vmFileName as well

--- boot/ova.nix
+++ boot/ova.nix
@@ -8,6 +8,7 @@
   virtualbox.params.usbehci = "off";
 
   virtualbox.vmDerivationName = "${if (config.nixiosk.localSystem.hostName or null) != null then config.nixiosk.localSystem.hostName else "nixiosk"}-ova-${config.system.nixos.label}-${pkgs.stdenv.hostPlatform.system}";
+  virtualbox.vmFileName = "${if (config.nixiosk.localSystem.hostName or null) != null then config.nixiosk.localSystem.hostName else "nixiosk"}-ova-${config.system.nixos.label}-${pkgs.stdenv.hostPlatform.system}.ova";
   virtualbox.vmName = "Nixiosk ${config.system.nixos.label} (${if (config.nixiosk.localSystem.hostName or null) != null then config.nixiosk.localSystem.hostName else "nixiosk"};${pkgs.stdenv.hostPlatform.system})";
 
   systemd.services.virtualbox-vmsvga =
