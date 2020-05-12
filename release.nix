81d88d9f7a42933891e47d272f5d86787ff34fef
Matthew Bauer <mjbauer95@gmail.com>
2020-05-12 15:20:05 -0500

    Use toplevel instead of images
    this is easier to cache

--- release.nix
+++ release.nix
@@ -40,75 +40,75 @@ in
     hardware = "raspberryPi0";
     program = { package = "retroarch"; executable = "/bin/retroarch"; };
     locale.timeZone = "America/New_York";
-  }).config.system.build.sdImage;
+  }).config.system.build.toplevel;
 
   retroPi4 = (boot {
     name = "retroPi4";
     hardware = "raspberryPi4";
     program = { package = "retroarch"; executable = "/bin/retroarch"; };
     locale.timeZone = "America/New_York";
-  }).config.system.build.sdImage;
+  }).config.system.build.toplevel;
 
   retroOva = (boot {
     name = "retroOva";
     hardware = "ova";
     program = { package = "retroarch"; executable = "/bin/retroarch"; };
-  }).config.system.build.virtualBoxOVA;
+  }).config.system.build.toplevel;
 
   retroIso = (boot {
     name = "retroIso";
     hardware = "iso";
     program = { package = "retroarch"; executable = "/bin/retroarch"; };
-  }).config.system.build.isoImage;
+  }).config.system.build.toplevel;
 
   epiphanyPi0 = (boot {
     name = "epiphanyPi0";
     hardware = "raspberryPi0";
     program = { package = "epiphany"; executable = "/bin/epiphany"; };
     locale.timeZone = "America/New_York";
-  }).config.system.build.sdImage;
+  }).config.system.build.toplevel;
 
   epiphanyPi4 = (boot {
     name = "epiphanyPi4";
     hardware = "raspberryPi4";
     program = { package = "epiphany"; executable = "/bin/epiphany"; };
     locale.timeZone = "America/New_York";
-  }).config.system.build.sdImage;
+  }).config.system.build.toplevel;
 
   demoPi0 = (boot {
     name = "demoPi0";
     hardware = "raspberryPi0";
     program = { package = "gtk3"; executable = "/bin/gtk3-demo"; };
     locale.timeZone = "America/New_York";
-  }).config.system.build.sdImage;
+  }).config.system.build.toplevel;
 
   demoPi1 = (boot {
     name = "demoPi1";
     hardware = "raspberryPi1";
     program = { package = "gtk3"; executable = "/bin/gtk3-demo"; };
     locale.timeZone = "America/New_York";
-  }).config.system.build.sdImage;
+  }).config.system.build.toplevel;
 
   demoPi2 = (boot {
     name = "demoPi2";
     hardware = "raspberryPi2";
     program = { package = "gtk3"; executable = "/bin/gtk3-demo"; };
     locale.timeZone = "America/New_York";
-  }).config.system.build.sdImage;
+  }).config.system.build.toplevel;
 
   demoPi3 = (boot {
     name = "demoPi3";
     hardware = "raspberryPi3";
     program = { package = "gtk3"; executable = "/bin/gtk3-demo"; };
     locale.timeZone = "America/New_York";
-  }).config.system.build.sdImage;
+  }).config.system.build.toplevel;
 
   demoPi4 = (boot {
     name = "demoPi4";
     hardware = "raspberryPi4";
     program = { package = "gtk3"; executable = "/bin/gtk3-demo"; };
     locale.timeZone = "America/New_York";
-  }).config.system.build.sdImage;
+  }).config.system.build.toplevel;
 
   # Currently broken, missing atomics in armv6l.
   # kodiPi1 = (boot {
@@ -123,26 +123,26 @@ in
     hardware = "raspberryPi2";
     program = { package = "kodi"; executable = "/bin/kodi"; };
     locale.timeZone = "America/New_York";
-  }).config.system.build.sdImage;
+  }).config.system.build.toplevel;
 
   kodiPi3 = (boot {
     name = "kodiPi3";
     hardware = "raspberryPi3";
     program = { package = "kodi"; executable = "/bin/kodi"; };
     locale.timeZone = "America/New_York";
-  }).config.system.build.sdImage;
+  }).config.system.build.toplevel;
 
   kodiPi4 = (boot {
     name = "kodiPi4";
     hardware = "raspberryPi4";
     program = { package = "kodi"; executable = "/bin/kodi"; };
     locale.timeZone = "America/New_York";
-  }).config.system.build.sdImage;
+  }).config.system.build.toplevel;
 
   kodiOva = (boot {
     name = "kodiOva";
     hardware = "ova";
     program = { package = "kodi"; executable = "/bin/kodi"; };
-  }).config.system.build.virtualBoxOVA;
+  }).config.system.build.toplevel;
 
 }
